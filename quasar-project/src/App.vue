a04e71c04e69ef7d2dada1df973d92e081410cde
syntax <alekseev.a@atbgroup.ru>
2024-01-09 17:00:42 +0300

    wip

--- quasar-project/src/App.vue
+++ quasar-project/src/App.vue
@@ -3,4 +3,47 @@
 </template>
 
 <script setup lang="ts">
+import { PushNotifications } from '@capacitor/push-notifications';
+import {onMounted} from "vue";
+
+const addListeners = async () => {
+  await PushNotifications.addListener('registration', token => {
+    console.info('Registration token: ', token.value);
+  });
+
+  await PushNotifications.addListener('registrationError', err => {
+    console.error('Registration error: ', err.error);
+  });
+
+  await PushNotifications.addListener('pushNotificationReceived', notification => {
+    console.log('Push notification received: ', notification);
+  });
+
+  await PushNotifications.addListener('pushNotificationActionPerformed', notification => {
+    console.log('Push notification action performed', notification.actionId, notification.inputValue);
+  });
+}
+
+const registerNotifications = async () => {
+  let permStatus = await PushNotifications.checkPermissions();
+
+  if (permStatus.receive === 'prompt') {
+    permStatus = await PushNotifications.requestPermissions();
+  }
+
+  if (permStatus.receive !== 'granted') {
+    throw new Error('User denied permissions!');
+  }
+
+  await PushNotifications.register();
+}
+
+const getDeliveredNotifications = async () => {
+  const notificationList = await PushNotifications.getDeliveredNotifications();
+  console.log('delivered notifications', notificationList);
+}
+
+onMounted(async () => {
+  await registerNotifications()
+})
 </script>
